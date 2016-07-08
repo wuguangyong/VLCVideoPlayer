3204ab52915714353b325aa5faf7ae250730b190
wgy <690802859@qq.com>
2016-07-08 14:55:39 +0800

    init demo

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# VLCVideoPlayer
+vlc实时流rtsp播放，可截图，可录制短视频 
+=======
+# VLCVidePlayer
+>>>>>>> init project
---
+++ src/com/example/vlcvideoplayer/VideoActivity.java
@@ -0,0 +1,269 @@
+package com.example.vlcvideoplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+
+import org.videolan.libvlc.IVLCVout;
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.util.AndroidUtil;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+public class VideoActivity extends Activity implements IVLCVout.Callback,
+		LibVLC.HardwareAccelerationError {
+	public final static String TAG = "LibVLCAndroidSample/VideoActivity";
+
+	public final static String LOCATION = "com.compdigitec.libvlcandroidsample.VideoActivity.location";
+
+	private String mFilePath;
+
+	// display surface
+	private SurfaceView mSurface;
+	private SurfaceHolder holder;
+
+	// media player
+	private LibVLC libvlc;
+	private MediaPlayer mMediaPlayer = null;
+	private int mVideoWidth;
+	private int mVideoHeight;
+	private final static int VideoSizeChanged = -1;
+
+	/*************
+	 * Activity
+	 *************/
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Receive path to play from intent
+		// /"rtsp://" + Camera.sIPAdd + "/xxxx.mp4"
+		// Intent intent = getIntent();
+		// mFilePath = intent.getExtras().getString(LOCATION);
+		mFilePath = "rtsp://192.168.1.254/xxx.mov";
+
+		Log.d(TAG, "Playing back " + mFilePath);
+
+		mSurface = (SurfaceView) findViewById(R.id.surface);
+		holder = mSurface.getHolder();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		setSize(mVideoWidth, mVideoHeight);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		createPlayer(mFilePath);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releasePlayer();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		releasePlayer();
+	}
+
+	/*************
+	 * Surface
+	 *************/
+	private void setSize(int width, int height) {
+		mVideoWidth = width;
+		mVideoHeight = height;
+		if (mVideoWidth * mVideoHeight <= 1)
+			return;
+
+		if (holder == null || mSurface == null)
+			return;
+
+		// get screen size
+		int w = getWindow().getDecorView().getWidth();
+		int h = getWindow().getDecorView().getHeight();
+
+		// getWindow().getDecorView() doesn't always take orientation into
+		// account, we have to correct the values
+		boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+		if (w > h && isPortrait || w < h && !isPortrait) {
+			int i = w;
+			w = h;
+			h = i;
+		}
+
+		float videoAR = (float) mVideoWidth / (float) mVideoHeight;
+		float screenAR = (float) w / (float) h;
+
+		if (screenAR < videoAR)
+			h = (int) (w / videoAR);
+		else
+			w = (int) (h * videoAR);
+
+		// force surface buffer size
+		holder.setFixedSize(mVideoWidth, mVideoHeight);
+
+		// set display size
+		LayoutParams lp = mSurface.getLayoutParams();
+		lp.width = w;
+		lp.height = h;
+		mSurface.setLayoutParams(lp);
+		mSurface.invalidate();
+	}
+
+	/*************
+	 * Player
+	 *************/
+
+	private void createPlayer(String media) {
+		releasePlayer();
+		try {
+			if (media.length() > 0) {
+				Toast toast = Toast.makeText(this, media, Toast.LENGTH_LONG);
+				toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0,
+						0);
+				toast.show();
+			}
+
+			// Create LibVLC
+			// TODO: make this more robust, and sync with audio demo
+			ArrayList<String> options = new ArrayList<String>();
+//			options.add("--aout=none");
+			options.add("--swscale-mode=0");
+			// options.add("--subsdec-encoding <encoding>");
+			options.add("--aout=opensles_android");
+			
+//			options.add("--audio-time-stretch"); // time stretching
+			options.add("-vvv"); // verbosity
+//			options.add("--network-caching=500");
+//			options.add(" --rtsp-caching=300");
+//			options.add(" --realrtsp-caching=300");
+//			options.add(" --clock-synchro=1");
+			libvlc = new LibVLC(options);
+			libvlc.setOnHardwareAccelerationError(this);
+			holder.setKeepScreenOn(true);
+			// Create media player
+			mMediaPlayer = new MediaPlayer(libvlc);
+			mMediaPlayer.setEventListener(mPlayerListener);
+
+			// Set up video output
+			final IVLCVout vout = mMediaPlayer.getVLCVout();
+			vout.setVideoView(mSurface);
+			// vout.setSubtitlesView(mSurfaceSubtitles);
+			vout.setWindowSize(640, 480);
+			vout.addCallback(this);
+			vout.attachViews();
+
+			Media m = new Media(libvlc, Uri.parse(media));
+m.setHWDecoderEnabled(true, false);
+m.addOption(":network-caching=450");
+m.addOption(":clock-jitter=0");
+m.addOption(":clock-synchro=0");
+			mMediaPlayer.setMedia(m);
+			mMediaPlayer.play();
+		} catch (Exception e) {
+			Toast.makeText(this, "Error creating player!", Toast.LENGTH_LONG)
+					.show();
+		}
+	}
+
+	// TODO: handle this cleaner
+	private void releasePlayer() {
+		if (libvlc == null)
+			return;
+		mMediaPlayer.stop();
+		final IVLCVout vout = mMediaPlayer.getVLCVout();
+		vout.removeCallback(this);
+		vout.detachViews();
+		holder = null;
+		libvlc.release();
+		libvlc = null;
+
+		mVideoWidth = 0;
+		mVideoHeight = 0;
+	}
+
+	/*************
+	 * Events
+	 *************/
+
+	private MediaPlayer.EventListener mPlayerListener = new MyPlayerListener(
+			this);
+
+	@Override
+	public void onNewLayout(IVLCVout vout, int width, int height,
+			int visibleWidth, int visibleHeight, int sarNum, int sarDen) {
+		if (width * height == 0)
+			return;
+
+		// store video size
+		mVideoWidth = width;
+		mVideoHeight = height;
+		setSize(mVideoWidth, mVideoHeight);
+	}
+
+	@Override
+	public void onSurfacesCreated(IVLCVout vout) {
+
+	}
+
+	@Override
+	public void onSurfacesDestroyed(IVLCVout vout) {
+
+	}
+
+	private static class MyPlayerListener implements MediaPlayer.EventListener {
+		private WeakReference<VideoActivity> mOwner;
+
+		public MyPlayerListener(VideoActivity owner) {
+			mOwner = new WeakReference<VideoActivity>(owner);
+		}
+
+		@Override
+		public void onEvent(MediaPlayer.Event event) {
+			VideoActivity player = mOwner.get();
+
+			switch (event.type) {
+			case MediaPlayer.Event.EndReached:
+				Log.d(TAG, "MediaPlayerEndReached");
+				player.releasePlayer();
+				break;
+			case MediaPlayer.Event.Playing:
+			case MediaPlayer.Event.Paused:
+			case MediaPlayer.Event.Stopped:
+			default:
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void eventHardwareAccelerationError() {
+		// Handle errors with hardware acceleration
+		Log.e(TAG, "Error with hardware acceleration");
+		this.releasePlayer();
+		Toast.makeText(this, "Error with hardware acceleration",
+				Toast.LENGTH_LONG).show();
+	}
+}
